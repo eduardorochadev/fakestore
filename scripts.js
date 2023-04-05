833315fb3e01b29cd413eb0b7caf34665bc2dd8b
chicoedumacedo <chicoedumacedo@gmail.com>
2023-04-05 11:11:52 -0300

    correção dos checkbox

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
-let lista = [];
+let listaDeProdutos = [];
 
 function buscarCategorias(){
     fetch("https://fakestoreapi.com/products/categories")
     .then(res => res.json())
     .then(listaCategorias => {
+
         listaCategorias.forEach(cada => {
             categorias.innerHTML += `   
-                <li class="$>
+                <li class="list-group-item">
                     <h6>
-                        <input name="${cada}" type="checkbox" onClick="filtro()">
+                        <input name="${cada.replace(" ","-")}" type="checkbox" onClick="filtro()">
                         <span>${cada}</span>
                     </h6>
                 </li>`
@@ -22,21 +23,20 @@ function buscarProdutos(){
     .then(res => res.json())
     .then(listaProdutos => {
         listaProdutos.forEach(cada => {
-            lista = listaProdutos
+            lista = listaDeProdutos
             products.innerHTML += `
-            <li>
+            <li class="${cada.category.replace(" ","-")}">
                 <div class="card">
-                    <picture>
-                        <img src="${cada.image}" alt="${cada.title}" class="card-img-top">
-                    </picture>
+                    <img src="${cada.image}" class="card-img-top" alt="">
                     <div class="card-body">
                         <h5 class="card-title" title="${cada.title}">${cada.title}</h5>
-                        <p class="card-text text-warp">${cada.description}</p>
-                        <h6 class="card-text"> $${cada.price} <h6>
-                        <a href="" class="btn btn-primary">Adicionar ao Carrinho</a>
+                        <div class="badge bg-secondary mb-2">${cada.category}</div>
+                        <p class="card-text text-wrap">${cada.description}</p>
+                        <a href="" class="btn btn-primary">Adicionar ao carrinho</a>
                     </div>
                 </div>
-            </li>`
+            </li>
+        `;
         })
     })
 }buscarProdutos()
@@ -44,16 +44,22 @@ function buscarProdutos(){
 
 function filtro ()
 {
-    let inputs = document.querySelectorAll('input.checked');
+    let inputs = document.querySelectorAll('input:checked');
     let inputs_nomes = [];
     for(let i = 0; i < inputs.length; i++){
         inputs_nomes.push(inputs[i].name)
     }
 
-    let lis = documet.querySelectorAll("#products li");
-    for(let i = 0; lis.length; i++){
+    let lis = document.querySelectorAll("#products li");
+    for(let i = 0; i < lis.length; i++){
         if(!lis[i].classList.contains(inputs_nomes.toString())){
-            lis[i}.Style.display = none
+            lis[i].style.display = 'none'
+        }else{
+            lis[i].style.display = 'block' 
+        }
+
+        if(inputs_nomes.length === 0){
+            lis[i].style.display = 'block'
         }
     }
 }
